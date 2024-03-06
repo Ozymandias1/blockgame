381fe6c6d83385de146ad214063aa361266ee936
Ozymandias1 <mrbaek86@gmail.com>
2024-03-07 02:10:59 +0900

    게임 조건 설정 주석 작업

--- Scripts/SetGameCondition.gd
+++ Scripts/SetGameCondition.gd
@@ -1,18 +1,23 @@
 extends CenterContainer
 
+#region 변수
 @onready var menu_controller = $"../../MenuController"
 @onready var gameplay = $"../Gameplay"
+#endregion
 
-# 보드 크기 옵션값 변경 시그널
+#region 보드 크기 옵션값 변경 시그널
 func _on_option_board_size_item_selected(index):
 	var board_size = index + 9
 	gameplay.board_size = board_size # 보드사이즈(그리드컬럼개수) 설정
-	
-# Play 버튼 클릭 시그널
+#endregion
+
+#region Play 버튼 클릭 시그널
 func _on_btn_play_pressed():
 	menu_controller.change_menu(Constants.MenuPage.Gameplay)
 	gameplay.gameplay_start()
+#endregion
 
-# Back 버튼 클릭 시그널
+#region Back 버튼 클릭 시그널
 func _on_btn_back_pressed():
 	menu_controller.change_menu(Constants.MenuPage.MainMenu)
+#endregion
