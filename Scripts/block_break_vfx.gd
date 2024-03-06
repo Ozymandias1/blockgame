2925a2a2c32f5d7b340222fe8aeed7d0745b555e
Ozymandias1 <mrbaek86@gmail.com>
2024-03-07 01:25:47 +0900

    블럭vfx, 콤보텍스트 주석작업

--- Scripts/block_break_vfx.gd
+++ Scripts/block_break_vfx.gd
@@ -1,7 +1,8 @@
 extends RigidBody2D
 
-# VisibleOnScreenNotifier2D의 화면 밖으로 나갔을때의 시그널 처리
+#region VisibleOnScreenNotifier2D 노드의 화면 밖으로 나갔을때의 시그널 처리
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	# 현재 노드를 부모로부터 제거하고 메모리 해제
 	self.get_parent().remove_child(self)
 	self.queue_free()
+#endregion
